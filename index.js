3a491d474158828d00543ee32ff2958f0cda4168
Andrianna <andrianna.odell@gmail.com>
2022-06-29 08:12:58 -0400

    commit tree 8b201c35cf385b5a08bccedecf02de5a3f5c3ad6

---
+++ index.js
@@ -0,0 +1,13 @@
+// define the function expression divide
+// divides two numbers
+const divide = function(num1, num2){
+    return num1 / num2;
+}
+
+// define the arrow function square
+// takes one parameter and multiplies it times itself
+const square = (num) => num * num;
+
+// define the arrow function add
+// takes two parameters and adds them together
+const add = (num1, num2) => num1 + num2;
